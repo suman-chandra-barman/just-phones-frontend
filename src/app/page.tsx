8173236889644be59685a94972f7231a5af35145
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2025-07-07 20:46:25 +0600

    Add Phones in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,39 @@
 "use client"
 
-// import { useGetAllPhonesQuery } from "@/redux/features/phones/phonesApi";
+import PhoneCart from "@/components/PhoneCart";
+import PhoneCartSkeleton from "@/components/PhoneCartSkeleton";
+import { useGetAllPhonesQuery } from "@/redux/features/phones/phonesApi";
+import { TPhone } from "@/types/common";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    // const { data, isLoading, isError } = useGetAllPhonesQuery({});
-    // console.log("Home page Phones Data:", data);
+  const {data, isLoading } = useGetAllPhonesQuery({});
+  const [phonesData, setPhonesData] = useState<TPhone[]>([]);
+
+  useEffect(() => {
+    if (data?.data) {
+      setPhonesData(data.data);
+    }
+  }, [data?.data]);
+
   return (
-   <>
-    Home Page
-   </>
+    <section className="bg-base-100 mt-10">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold">New Arrival</h1>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 my-10">
+          {isLoading ? (
+            Array.from({ length: 10 }).map((_, index) => (
+              <PhoneCartSkeleton key={index} />
+            ))
+          ) : phonesData.length > 0 ? (
+            phonesData.map((phone) => <PhoneCart key={phone._id} phone={phone}/>)
+          ) : (
+            <p className="text-center text-gray-500 col-span-full">
+              No phones available at the moment.
+            </p>
+          )}
+        </div>
+      </div>
+    </section>
   );
 }
