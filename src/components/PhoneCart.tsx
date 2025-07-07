8ab80e3e8d9451e16679bc59928b428e19948494
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2025-07-07 18:18:27 +0600

    Daynamic phone cart

--- src/components/PhoneCart.tsx
+++ src/components/PhoneCart.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { TPhone } from "@/types/common";
+import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineHeart } from "react-icons/ai";
 
-const PhoneCart = ({phone}) => {
+const PhoneCart = ({phone} : {phone:TPhone}) => {
   console.log("Phone Cart Data:", phone);
-  
+
   return (
     <div className="group relative card bg-white rounded-xl overflow-hidden transition-shadow duration-300 shadow-[0_4px_16px_rgba(0,0,0,0.08)] hover:shadow-[0_8px_28px_rgba(0,0,0,0.12)]">
       {/* Wishlist*/}
@@ -18,19 +20,27 @@ const PhoneCart = ({phone}) => {
 
       <Link href={`/phones/${1}`} className="flex flex-col h-full">
         <figure className="p-4">
-          <img
-            src={"https://www.mobiledokan.com/media/xiaomi-poco-f7-silver-official-image.webp"}
-            alt={"Tecno Spark 40"}
+          <Image
+            src={phone.image}
+            alt={phone.name}
+            height={160}
+            width={240}
             className="rounded-xl h-40 object-contain transition-transform duration-500 group-hover:scale-120"
           />
         </figure>
 
         <div className="card-body pt-0">
-          <h2 className="card-title text-lg">Tecno Spark 40</h2>
-          <p className="text-[#ff4c11] text-lg">
-            <span className="font-bold">৳ </span>
-            <span className="font-semibold">{1000.00}</span>
-          </p>
+          <h2 className="card-title text-lg">{phone.name}</h2>
+          <div className="flex items-center gap-2">
+            <span className="text-[#ff4c11] text-lg">
+              <span className="font-bold">৳ </span>
+              <span className="font-semibold">{phone.price}</span>
+            </span>
+            {phone.previousPrice  && <span className="line-through text-gray-500 text-sm">
+              <span className="font-bold">৳ </span>
+              <span className="font-semibold">{phone.previousPrice}</span>
+            </span>}
+          </div>
         </div>
       </Link>
       {/* Add to Cart Button */}
---
+++ src/types/common.ts
@@ -0,0 +1,31 @@
+export type TPhone = {
+  _id?: string;
+  name: string;
+  brand: string;
+  image: string;
+  description: string;
+  marketStatus: "In Stock" | "Out of Stock" | "Coming Soon";
+  releaseDate: Date;
+  updatedOn: Date;
+  isOfficial: boolean;
+  price: number;
+  previousPrice?: number;
+  countInStock: number;
+  rating?: number;
+  numReviews?: number;
+  isDeleted?: boolean;
+  specifications?: {
+    os?: string;
+    storage?: string;
+    ram?: string;
+    mainCamera?: string;
+    frontCamera?: string;
+    display?: string;
+    battery?: string;
+    fingerprintSensor?: string;
+    charging?: string;
+    protection?: string;
+    connectivity?: string;
+    glassProtection?: string;
+  };
+};
